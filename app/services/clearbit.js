d80de3c8c0b6aefe0a54c89b5b6c40f10f3ef1ad
Thomas Brus <thomas.brus@me.com>
2018-03-20 21:14:20 +0100

    Added clearbit autocomplete service

---
+++ app/services/clearbit.js
@@ -0,0 +1,11 @@
+import Service from '@ember/service';
+import { service } from 'ember-decorators/service';
+
+export default class ClearbitService extends Service {
+  @service ajax
+
+  autocomplete(query) {
+    const url = 'https://autocomplete.clearbit.com/v1/companies/suggest';
+    return this.get('ajax').request(url, { data: { query } });
+  }
+}
