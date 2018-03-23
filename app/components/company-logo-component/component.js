7eaca15b59f41091f7008926a87fc73bb4fd45fa
Thomas Brus <thomas.brus@me.com>
2018-03-23 01:46:56 +0100

    Show spinner until company logo is fully loaded

--- app/components/company-logo-component/component.js
+++ app/components/company-logo-component/component.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
+import { action, computed } from '@ember-decorators/object';
+import { reads, and} from '@ember-decorators/object/computed';
 import { service } from '@ember-decorators/service';
-import { task } from 'ember-concurrency';
+import { task, waitForEvent } from 'ember-concurrency';
 
 export default class CompanyLogoComponent extends Component {
   @service clearbit
@@ -10,23 +12,38 @@ export default class CompanyLogoComponent extends Component {
 
   didInsertElement() {
     super.didInsertElement(...arguments);
-    this.get('fetchLogo').perform();
+    this.get('loadLogo').perform();
   }
 
-  fetchLogo = task(function * () {
+  @reads('loadLogo.isRunning') spinnerVisible
+
+  @and('resolveLogo.last.isSuccessful', 'loadLogo.isRunning') logoLoading
+  @reads('loadLogo.last.isSuccessful') logoLoaded
+  @reads('resolveLogo.lastSuccessful.value') logoSrc
+
+  @computed('loadLogo.{isRunning,last.isCanceled}', 'logoSrc')
+  get placeholderVisible() {
+    if (this.get('loadLogo.isRunning')) { return false; }
+    return this.get('loadLogo.last.isCanceled') || !this.get('logoSrc');
+  }
+
+  resolveLogo = task(function * () {
     let results = yield this.get('clearbit').autocomplete(this.get('name'));
     let compare = (str1, str2) => str1.toLowerCase() === str2.toLowerCase();
     let company = results.find(c => compare(c.name, this.get('name'))) || results[0];
 
-    if (!company || !company.logo) {
-      return null;
-    }
+    if (!company || !company.logo) { return null; }
 
-    try {
-      yield this.get('proxy').request(company.logo, { method: 'HEAD' });
-      return company.logo;
-    } catch(e) {
-      return null;
-    }
-  })
+    return company.logo;
+  }).restartable()
+
+  loadLogo = task(function * () {
+    let logo = yield this.get('resolveLogo').perform();
+    if (logo) { yield waitForEvent(this, 'load'); }
+  }).restartable()
+
+  @action
+  onLoad() {
+    this.trigger('load');
+  }
 }
